05cd38229518ee7b0b63afd1740fda6a061de2ec
Pujakumari1511 <puja.kumari6494@gmail.com>
2024-10-23 09:34:42 +0300

    Enhancing mobile menu, when clicking on Navbar

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,5 +1,6 @@
 const mobileMenuButton = document.querySelector(".mobile-menu");
 const navList = document.querySelector("nav ul");
+const navItems = document.querySelectorAll("nav ul li a");
 const backToTop = document.querySelector("#backTop");
 
 backToTop.addEventListener("click", () => {
@@ -11,7 +12,7 @@ window.onscroll = function (){scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.querySelector('header').style.backdropFilter = "blur(10px)";
+        //document.querySelector('header').style.backdropFilter = "blur(10px)";
         document.querySelector('header .logo h1').style.fontSize = "22px";
         document.querySelector('header').style.backgroundColor = "#05590c5c";
         document.querySelector('header').style.height = "100px";
@@ -24,7 +25,31 @@ function scrollFunction() {
 }
 
 const toggleMenu = () => {
-    navList.classList.toggle("responsive");   
+    navList.classList.toggle("open");
 }
 
 mobileMenuButton.addEventListener("click", toggleMenu);
+
+// if the screen width is mobile size, close the menu options on click
+for (let navItem of navItems) {
+    navItem.addEventListener("click", function() {
+        if (window.matchMedia("(max-width: 600px)").matches) {
+            navList.classList.toggle("open");
+        }
+    })
+}
+
+// Function to check if the screen width is mobile size
+function checkScreenSize() {
+    if (window.matchMedia("(max-width: 600px)").matches) {
+        navList.classList.add('responsive'); // Add the responsive class only for small screens
+    } else {
+        navList.classList.remove('responsive'); // Remove it when the screen size is larger
+    }
+}
+
+// Add an event listener to handle screen resize
+window.addEventListener('resize', checkScreenSize);
+
+// Initial check when the page loads
+checkScreenSize();
